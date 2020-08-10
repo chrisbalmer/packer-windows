614f353b2b663d3e098a7707b3cf86039ae29b4b
Chris Balmer <git@chrisbalmer.io>
2020-08-09 21:24:56 -0400

    Cleaned up powershell and renamed second stage

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ export PACKER_LOG_PATH=/tmp/windows10-$BUILD_DATE.log
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 packer build -var-file=./vars/$1.json $2-base.json
-packer build -var-file=./vars/$1.json $2-ova.json 
+packer build -var-file=./vars/$1.json $2-cloud.json 
